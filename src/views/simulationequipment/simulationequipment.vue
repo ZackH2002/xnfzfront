893d1be0581a53009a9ce2ccffeb4e2d3eb6b52b
hdh25490 <13896922098@163.com>
2023-08-11 15:55:42 +0800

    引入echarts

--- src/views/simulationequipment/simulationequipment.vue
+++ src/views/simulationequipment/simulationequipment.vue
@@ -170,7 +170,7 @@
       <span>是否要删除改仿真设备</span>
       <span slot="footer" class="dialog-footer">
         <el-button type="primary" @click="deleteEquipment(simulationEquipment)">确 定</el-button>
-        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button @click="dialogNoticeVisible = false">取 消</el-button>
       </span>
     </el-dialog>
   </div>
